8647f5636968c9f8c584fc919b85e6a345701b4d
Séverin Lemaignan <severin.lemaignan@laas.fr>
2009-09-29 18:33:17 +0200

    Release 0.5.9 : removal of dependencies on YARP and JSON, replaced by a socket-based implementation

--- src/laas/openrobots/ontology/OroServer.java
+++ src/laas/openrobots/ontology/OroServer.java
@@ -120,7 +120,7 @@ public class OroServer implements IServiceProvider {
 	 * The default configuration file (set to {@value}).
 	 */
 	public static final String DEFAULT_CONF = "etc/oro-server/oro.conf";
-	public static final String VERSION = "0.5.0"; //version: major.minor.build (minor -> add/removal of feature, build -> bug correction)
+	public static final String VERSION = "0.5.9"; //version: major.minor.build (minor -> add/removal of feature, build -> bug correction)
 	
 	public static final Date SERVER_START_TIME = new Date();
 
