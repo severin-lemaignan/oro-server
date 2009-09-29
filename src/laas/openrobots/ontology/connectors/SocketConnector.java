fdd3d08c53e41b1aadbe8ca4e013d9b5459a2b97
Séverin Lemaignan <severin.lemaignan@laas.fr>
2009-09-29 18:32:08 +0200

    Completion of the implementation of SocketConnector. Some bugs probably remain, but it should be feature complete.

--- src/laas/openrobots/ontology/connectors/SocketConnector.java
+++ src/laas/openrobots/ontology/connectors/SocketConnector.java
@@ -4,12 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import laas.openrobots.ontology.Pair;
 import laas.openrobots.ontology.exceptions.OntologyConnectorException;
@@ -29,14 +37,21 @@ public class SocketConnector implements IConnector, Runnable {
 	 */
 	class ClientWorker implements Runnable {
 		  private Socket client;
+		  private String name;
+		  
+		  private Boolean keepOnThisWorker = true;
 		  
 		  ClientWorker(Socket client) {
 		   this.client = client;
+		   this.name = "ClientWorker " + client.toString();
 		  }
 
+		  public String getName(){
+		  	return name;
+		  }
 		  
 		  public void run(){
-		    String request;
+
 		    BufferedReader in = null;
 		    PrintWriter out = null;
 		    try{
@@ -47,9 +62,18 @@ public class SocketConnector implements IConnector, Runnable {
 		      System.exit(-1);
 		    }
 
-		    while(keepOn){
+		    while(keepOn && keepOnThisWorker){
 		      try{
-		        request = in.readLine().trim();
+		    	 
+		    	ArrayList<String> request = new ArrayList<String>();
+		    	String line;
+		    	while (true) {
+		    		line = in.readLine().trim();
+		    		if (line.equalsIgnoreCase("#end#"))
+		    			break;
+		    		else request.add(line);
+		    	}
+		    	
 		        //Send data back to client
 		        
 		        String res = handleRequest(request);
@@ -62,48 +86,250 @@ public class SocketConnector implements IConnector, Runnable {
 		    }
 		  }
 		  
-		  public String handleRequest(String request) {
+		  public String handleRequest(ArrayList<String> request) {
 			  
 			  boolean methodFound = false;
-			  String[] tokens = request.split("\\s");
+			  
+			  String result = "error\n\n";
+			  String queryName = request.get(0);
+			  
 			  
 			  System.out.println(" * Got a request: ");
-			  for (String s : tokens)
+			  for (String s : request)
 			         System.out.println(s);
 
 
-	    	if (tokens[0].equalsIgnoreCase("close")){
-	    		System.out.println(" * Closing communication");
-	    		//TODO : Fermer le socket
+	    	if (queryName.equalsIgnoreCase("close")){
+	    		System.out.println(" * Closing a communication with a client");
+	    		
+	    		keepOnThisWorker = false;
+	    		
 	    		return "Closing now!";
 	    	}
 	    	else
 	    	{
-	    		/******* Externally registred methods ********/
+	    		/******* Iterate on registred methods ********/
 	    		for (Pair<String,String> name : registredServices.keySet()){
 	    			
 	    			Method m = registredServices.get(name).getLeft();
 	    			Object o = registredServices.get(name).getRight();
 	    			
-	    	    	if (name.getLeft().equalsIgnoreCase(tokens[0]) &&
+	    	    	if (name.getLeft().equalsIgnoreCase(queryName) &&
 	    	    			(
-    	    				(tokens.length == 1) ? 
+    	    				(request.size() == 1) ? 
     	    						m.getParameterTypes().length == 0 :
-    	    						(m.getParameterTypes().length == tokens.length - 1)
+    	    						(m.getParameterTypes().length == request.size() - 1)
     	    				)
     	    			)
 	    	    	{
 	    	    		methodFound = true;
+	    	    		boolean invokationDone = false;
 	    	    		
-	    	    		return "Method " + tokens[0] + " found. Youpi.";
+	    	    		try {
+	    	    			
+	    	    			
+	    	    			result = "ok\n";
+	    	    			
+	    	    			Object[] args = new Object[m.getParameterTypes().length];
+	    	    			
+	    	    			int i = 0;	    	    			
+	    	    			if (request.size() > 1) {
+		    	    			for (Class<?> param : m.getParameterTypes()) {
+		    	    				args[i] = deserialize(request.get(i + 1), param);
+		    	    				i++;
+		    	    			}
+	    	    			}
+	    	    			
+	    	    			if (m.getReturnType() == void.class)
+	    	    			{
+	    	    				if (m.getParameterTypes().length == 0) m.invoke(o); else m.invoke(o, args);
+	    	    				invokationDone = true;
+	    	    			}
+	    	    			if (m.getReturnType() == String.class || m.getReturnType() == Double.class || m.getReturnType() == Integer.class || m.getReturnType() == Boolean.class || m.getReturnType() == Float.class) {
+	    	    				result += (m.getParameterTypes().length == 0) ? m.invoke(o).toString() : m.invoke(o, args).toString();
+	    	    				invokationDone = true;
+	    	    			} else {
+	    	    				
+	    	    				List<Class<?>> rTypes = new ArrayList<Class<?>>();
+	    	    				
+	    	    				rTypes.add(m.getReturnType());
+	    	    				
+	    	    				rTypes.addAll(Arrays.asList(m.getReturnType().getInterfaces()));
+	    	    				
+
+		    	    			for (Class<?> rType : rTypes ) {
+		    	    				if (rType == Serializable.class) {
+		    	    					result += (m.getParameterTypes().length == 0) ? m.invoke(o).toString() : m.invoke(o, args).toString();
+		    	    					invokationDone = true;
+		    	    					break;
+		    	    				}
+		    	    				
+		    	    				if (rType == Map.class) {
+		    	    					result += mapToString(((Map<?, ?>)(((m.getParameterTypes().length == 0) ? m.invoke(o) : m.invoke(o, args)))));
+		    	    					invokationDone = true;
+		    	    					break;
+		    	    				}
+		    	    				
+		    	    				if (rType == Set.class) {
+		    	    					result += setToString(((Set<?>)(((m.getParameterTypes().length == 0) ? m.invoke(o) : m.invoke(o, args)))));
+		    	    					invokationDone = true;
+		    	    					break;
+		    	    				}
+		    	    				
+		    	    				if (rType == List.class) {
+		    	    					result += listToString(((List<?>)(((m.getParameterTypes().length == 0) ? m.invoke(o) : m.invoke(o, args)))));
+		    	    					invokationDone = true;
+		    	    					break;
+		    	    				}
+	
+		    	    			}
+	    	    			}
+	    	    			
+	    	    			if (!invokationDone) {
+	    						System.err.println("ERROR while executing the request: no way to serialize the return value of method '"+ queryName + "' (return type is " + m.getReturnType().getName() + ").");
+	    						result ="error\n" +
+	    								"\n" +
+	    								"No way to serialize return value of method '" + queryName + "' (return type is " + m.getReturnType().getName() + ").";	
+	    	    			}
+	    	    			
+	    	    			
+						} catch (IllegalArgumentException e) {
+							System.err.println("ERROR while executing the request '" + queryName + "': " + e.getClass().getName() + " -> " + e.getLocalizedMessage());
+							result = "error\n" +
+									e.getClass().getName() + "\n" +
+									e.getLocalizedMessage().replace("\"", "'");
+							
+						} catch (ClassCastException e) {
+							System.err.println("ERROR while executing the request '" + queryName + "': " + e.getClass().getName() + " -> " + e.getLocalizedMessage());
+							result = "error\n" +
+							e.getClass().getName() + "\n" +
+							e.getLocalizedMessage().replace("\"", "'");
+							
+						} catch (IllegalAccessException e) {
+							System.err.println("ERROR while executing the request '" + queryName + "': " + e.getClass().getName() + " -> " + e.getLocalizedMessage());
+							result = "error\n" +
+							e.getClass().getName() + "\n" +
+							e.getLocalizedMessage().replace("\"", "'");	
+							
+						} catch (InvocationTargetException e) {
+							System.err.println("ERROR while executing the request '" + queryName + "': " + e.getCause().getClass().getName() + " -> " + e.getCause().getLocalizedMessage());
+							result = "error\n" + 
+									e.getCause().getClass().getName() + "\n" +
+									e.getCause().getLocalizedMessage().replace("\"", "'");							
+						
+						}
+						
+						break;
 	    	    		
 	    	    	}
 	    		}
-	    		return "Method not found. Bouhouh.";
+	    		if (!methodFound){
+					System.err.println("ERROR while executing the request: method \""+ queryName + "\" (with " + (request.size() - 1) + " parameters) not implemented by the ontology server.");
+					result = "error\n" +
+							"NotImplementedException\n" +
+							"Method " + queryName + " (with " + (request.size() - 1) + " parameters) not implemented by the ontology server.";						
+	    	    }
+	    		
+	    		return result + "\n#end#";
 	    	}
-			  
-			  
 		  }
+	    	
+    	private <V> String listToString(List<V> list) {
+    		String str = "[";
+    		for (V v : list) {
+    			str += v.toString() + ",";
+    		}
+    		
+    		str = str.substring(0, str.length() - 1) + "]";
+    		
+    		return str;
+    	}
+
+    	private <V> String setToString(Set<V> list) {
+    		String str = "[";
+    		for (V v : list) {
+    			str += v.toString() + ",";
+    		}
+    		
+    		str = str.substring(0, str.length() - 1) + "]";
+    		
+    		return str;
+    	}
+
+    	private <K, V> String mapToString(Map<K, V> map) {
+    		String str = "{";
+    		for (Entry<K, V> es : map.entrySet()) {
+    			str += es.getKey().toString() + ":" + es.getValue().toString() + ",";
+    		}
+    		
+    		str = str.substring(0, str.length() - 1) + "}";
+    		
+    		return str;
+    	}
+			  
+
+		  private Object deserialize(String val, Class<?> type) {
+				//not typed because of Method::invoke requirements <- that's what I call a bad excuse
+				
+					if (type == String.class)
+						return val;
+							
+					if (type == Integer.class)
+						return Integer.parseInt(val);
+					
+					if (type == Double.class)
+						return Double.parseDouble(val);
+					
+					if (type == Boolean.class)
+						return Boolean.parseBoolean(val);
+					
+					//assumes it's a list or map
+						
+					boolean isValidMap = true;
+					boolean isValidSet = true;
+					
+					//First, inspect the string to determine the type.
+					//If it starts and ends with {}, it's a map
+					//If it starts and ends with [], it's a set
+					if ( !val.substring(0, 1).equals("{") || !val.substring(val.length() - 1, val.length()).equals("}"))
+							isValidMap = false;
+					
+					if ( !val.substring(0, 1).equals("[") || !val.substring(val.length() - 1, val.length()).equals("]"))
+							isValidSet = false;
+					
+					val = val.substring(1, val.length() - 1); //remove the [] or {}
+					
+					//checks that every elements of the map is made of tokens separated by :
+					for (String s : val.split(","))
+						if (!isValidMap || !s.contains(":"))
+							isValidMap = false;
+					
+					assert(!(isValidMap && isValidSet));
+					
+					//if the string looks like a map and a map is indeed expected...
+					if (isValidMap && Map.class.isAssignableFrom(type)){
+						Map<String, String> result = new HashMap<String, String>();
+						for (String s : val.split(","))
+							result.put(s.trim().split(":")[0], s.trim().split(":")[1]);
+						return result;
+					}					
+					//if the string looks like a set and a set of a list is indeed expected...
+					else if (isValidSet && Set.class.isAssignableFrom(type)){
+						Set<String> result = new HashSet<String>();
+						for (String s : val.split(","))
+							result.add(s.trim());
+						return result;
+					}					
+					//if the string looks like a set and a list of a list is indeed expected...
+					else if (isValidSet && List.class.isAssignableFrom(type)){
+						List<String> result = new ArrayList<String>();
+						for (String s : val.split(","))
+							result.add(s.trim());
+						return result;
+					}
+					
+					else throw new IllegalArgumentException("Unable to deserialize the parameter of the query! (a " + type.getName() + " was expected, received \"" + val + "\")");
+			}
 	}
 		
 	public SocketConnector(
@@ -161,7 +387,7 @@ public class SocketConnector implements IConnector, Runnable {
 	      ClientWorker w;
 	      try{
 	        w = new ClientWorker(server.accept());
-	        Thread t = new Thread(w);
+	        Thread t = new Thread(w, w.getName());
 	        t.start();
 	      } catch (IOException e) {
 	        System.err.println("Accept failed on port " + port);
