89c48f1f5b706e50aaa1d81b6a8fe223a0df4aa3
Séverin Lemaignan <severin.lemaignan@laas.fr>
2009-11-04 19:32:57 +0100

    Add of a new exception type for Agents

---
+++ src/laas/openrobots/ontology/exceptions/AgentNotFoundException.java
@@ -0,0 +1,9 @@
+package laas.openrobots.ontology.exceptions;
+
+public class AgentNotFoundException extends OntologyServerException {
+
+	public AgentNotFoundException(String message) {
+		super(message);
+	}
+	
+}
