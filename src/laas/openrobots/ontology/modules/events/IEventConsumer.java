84168edebd67438e15db55f26c22e938d25caa13
Séverin Lemaignan <severin.lemaignan@laas.fr>
2009-11-25 12:30:28 +0100

    Minor doc improvement

--- src/laas/openrobots/ontology/modules/events/IEventConsumer.java
+++ src/laas/openrobots/ontology/modules/events/IEventConsumer.java
@@ -1,5 +1,11 @@
 package laas.openrobots.ontology.modules.events;
 
+/**
+ * This interface is intended to be implemented by classes that are expected to
+ * be notified when some event occurs, thus consumming the event.
+ * @author slemaign
+ *
+ */
 public interface IEventConsumer {
 
 	void consumeEvent(OroEvent e);
