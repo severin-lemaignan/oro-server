e620c23e548812a538e9b34db9d98b0dc60ed5aa
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-01-21 15:40:55 +0100

    Added the 'log_player' tool
    This python script is able to read the oro-server logs (produced in DEBUG mode)
    and 'replay' them by re-executing the same requests on the server.
    It can reproduce several clients, but the exact timing is not kept.

---
+++ tools/log_player.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+
+import sys, re
+from pyoro import Oro
+
+threads_list = {}
+
+i=0
+
+with open(sys.argv[1], 'r') as f:
+    for l in f:
+        i += 1
+        cmd_line = re.search("(?<=thread )\d+",l)
+        try:
+            thread_id = cmd_line.group(0)
+            req = re.search("(?<=Got incoming request: ).+", l).group(0)
+            if thread_id not in threads_list.keys():
+                print "New thread " + thread_id
+                threads_list[thread_id] = Oro("localhost", 6969)
+                
+            print "("+str(i)+") Thread " + thread_id + " sending " + req
+            eval("threads_list[thread_id]."+ req)
+        except AttributeError:
+            #Not a log line with a request content
+            pass
+
+for key, kb in threads_list.items():
+    kb.close()
