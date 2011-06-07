4953221e59520d38feb41bfd5a14471bb2528f89
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-06-07 18:45:24 +0200

    Several changes to log_player. Mostly cosmetic.
    - now display a percentage of _actually_ processed requests
    - threads are started only when the full log is parsed. This will cause some inconsistencies compared to the original
     execution path. This can be improved by using the (millisecond accurate) timestamps.

--- tools/log_player.py
+++ tools/log_player.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
 import sys, re
 from pyoro import Oro, OroServerError
@@ -11,31 +11,33 @@ PORT = 6969
 
 threads_list = {}
 
+nb_lines = 0
+current_line = 0
 
 class Client(Thread):
-    def __init__(self, oro):
+    def __init__(self, oro, id):
         print "[NEW THREAD CREATED]"
         Thread.__init__(self)
         self.req = Queue()
-        self.running = True
         self.oro = oro
-        self.start()
-
-    def __del__(self):
-        self.oro.close()
+        self.id = id
 
     def run(self):
-        while (not self.req.empty() or self.running):
+        global current_line
+        while (not self.req.empty()):
             req = self.req.get() #block until a request arrives
+            current_line += 1
+            print("[" + str(current_line * 100 / nb_lines) + "% - THREAD " + str(self.id) + " SENDING REQUEST " + req + "]")
             eval("self.oro." + req)
-        print("[LEAVING THE THREAD]")
+        print("[LEAVING THE THREAD " + str(self.id) + "]")
+        self.oro.close()
 
 
 i=0
 
 with open(sys.argv[1], 'r') as f:
     lines = f.readlines()
-    nb_line = len(lines)
+    nb_lines = len(lines)
     for l in lines:
         i += 1
         cmd_line = re.search("(?<=thread )\d+",l)
@@ -44,11 +46,10 @@ with open(sys.argv[1], 'r') as f:
             req = re.search("(?<=Got incoming request: ).+", l).group(0)
             if thread_id not in threads_list.keys():
                 print "New thread " + thread_id
-                threads_list[thread_id] = Client(Oro(HOST, PORT))
-                
-            print "(" + str(i*100/nb_line) +  "% - l."+str(i)+") Thread " + thread_id + " sending " + req
+                threads_list[thread_id] = Client(Oro(HOST, PORT), thread_id)
+
             try:
-                print "[SENDING REQUEST " + req + "]"
+                print "(" + str(i*100/nb_lines) +  "% - l."+str(i)+") Adding [" + req + "] to thread " + thread_id
                 threads_list[thread_id].req.put(req)
             except SyntaxError as e:
                 print "[SYNTAX ERROR] -> " + e.text + " @ " + str(e.offset)
@@ -58,7 +59,12 @@ with open(sys.argv[1], 'r') as f:
             #Not a log line with a request content
             pass
 
-for key, t in threads_list.items():
-    t.running = False
+print "[PARSING OF THE LOG FINISHED. WAITING FOR COMPLETION]\n\n\n"
 
-print "[PARSING OF THE LOG FINISHED]"
+for key, t in threads_list.items():
+    t.start()
+
+for key, t in threads_list.items():
+    t.join()
+
+print "[DONE. LOG REPLAYED.]"
